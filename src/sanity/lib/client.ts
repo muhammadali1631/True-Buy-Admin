5073871029621f8ff1dc3f47df42890174d448c7
Ali Shahzad <160423266+muhammadali1631@users.noreply.github.com>
2025-02-05 01:43:13 +0500

    done

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,7 +4,7 @@ import { createClient } from 'next-sanity'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2025-02-03' ,
+  apiVersion: 'vX' ,
   useCdn: true, 
   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
 })
